9987b5b414fd12321cad73ed9567e7b3ee6c8f2e
Patrick Thompson <patrick@pthompson.org>
2019-11-15 11:14:36 -0800

    remove extraneous module

--- lib/demo_web/live/counter_live.ex
+++ lib/demo_web/live/counter_live.ex
@@ -1,35 +1,3 @@
-defmodule DemoWeb.CountersLive do
-  use Phoenix.LiveView
-
-  def render(assigns) do
-    ~L"""
-    <%= inspect(@counters) %>
-    <button phx-click="shuffle">shufftle</button>
-    <%= for {{id, count}, index} <- Enum.with_index(@counters) do %>
-      <%= if index == 0 do %>
-        <blockquote>
-          <div>
-            <pre>
-              <%= id %><%= live_render(@socket, DemoWeb.CounterLive, id: "counter-#{id}", session: %{val: count}) %>
-            <pre>
-          <div>
-        </blockquote>
-      <% else %>
-        <%= id %><%= live_render(@socket, DemoWeb.CounterLive, id: "counter-#{id}", session: %{val: count}) %>
-      <% end %>
-    <% end %>
-    """
-  end
-
-  def mount(_session, socket) do
-    {:ok, assign(socket, counters: [{1, 1}, {2, 2}, {3, 3}])}
-  end
-
-  def handle_event("shuffle", _, socket) do
-    {:noreply, assign(socket, counters: IO.inspect(Enum.shuffle(socket.assigns.counters)))}
-  end
-end
-
 defmodule DemoWeb.CounterLive do
   use Phoenix.LiveView
   alias DemoWeb.Router.Helpers, as: Routes
